8eef7c7c95115d3e3fc0884f7dca0d32b915f87f
antoprince001 <antoprince001@gmail.com>
2023-10-28 15:13:54 +0530

    Update bridge pattern example

--- situational/bridge_pattern/app.py
+++ situational/bridge_pattern/app.py
@@ -3,14 +3,17 @@ class ColorImplementation:
     def apply_color(self):
         pass
 
+
 # Concrete implementations for colors
 class RedColor(ColorImplementation):
     def apply_color(self):
         return "Red"
 
+
 class GreenColor(ColorImplementation):
     def apply_color(self):
         return "Green"
+
 
 # Abstraction for shapes
 class Shape:
@@ -20,14 +23,17 @@ class Shape:
     def apply_color(self):
         return f"Shape filled with {self.color_implementation.apply_color()}"
 
+
 # Concrete shapes
 class Circle(Shape):
     def draw(self):
         return f"Circle: {self.apply_color()}"
 
+
 class Square(Shape):
     def draw(self):
         return f"Square: {self.apply_color()}"
+
 
 # Client code
 red_color = RedColor()
